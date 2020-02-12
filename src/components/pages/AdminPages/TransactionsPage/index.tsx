5e6a49836e3700f643a31fa2a5226a12c4fa9f0e
Sinan Shaban <sinan.zikri@gmail.com>
2020-02-13 00:39:21 +0200

    Full height layout

--- src/components/pages/AdminPages/TransactionsPage/index.tsx
+++ src/components/pages/AdminPages/TransactionsPage/index.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Box, Grid } from "@material-ui/core";
 import useStyles from "./styles";
-import { useWalletsQuery, useTransactionsLazyQuery, Wallet, Transaction } from "api";
+import {
+  useWalletsQuery,
+  useTransactionsLazyQuery,
+  Wallet,
+  Transaction,
+  TransactionType
+} from "api";
 import TransactionsTable from "components/organisms/transactions-table";
 import Modal from "components/molecules/modal";
 import TransactionForm from "components/organisms/transaction-form";
+import Chart from "react-google-charts";
+import SummaryBox from "components/molecules/summary-box";
 
 const TransactionsPage = () => {
   const classes = useStyles();
@@ -33,17 +41,62 @@ const TransactionsPage = () => {
     const bDate = new Date(b.date);
 
     return aDate > bDate ? -1 : bDate > aDate ? 1 : 0;
-  })
+  });
+
+  const currentDate: Date = new Date();
+  let backDate: Date = new Date();
+  backDate.setDate(currentDate.getDate() - 5);
+  const flowChart = [];
+
+  while (backDate <= currentDate) {
+    let total = 0;
+    transactions.forEach((transaction: any) => {
+      const date = new Date(transaction.date);
+      total +=
+        transaction.type === TransactionType.Expense &&
+        date.getDate() === backDate.getDate() &&
+        date.getMonth() === backDate.getMonth() &&
+        date.getFullYear() === backDate.getFullYear()
+          ? transaction.value
+          : 0;
+    });
+
+    flowChart.push([backDate.toLocaleDateString(), total]);
+    backDate.setDate(backDate.getDate() + 1);
+    total = 0;
+  }
+  const flowColumns = ['Date', 'Money'];
+
+  console.log([flowColumns, ...flowChart]);
 
   return (
     <Box className={classes.main} p={10}>
-      <Grid container>
+      <Grid container spacing={5}>
         <Grid item xs={12} md={6} lg={6}>
           <TransactionsTable
             wallets={wallets}
             transactions={transactions}
             onNewClick={() => setNewModalIsOpen(true)}
           />
+        </Grid>
+        <Grid item xs={12} md={6} lg={6}>
+          <SummaryBox header="Spending flow">
+            <Chart
+              width={"100%"}
+              height={"300px"}
+              chartType="LineChart"
+              loader={<div>Loading Chart</div>}
+              data={[flowColumns, ...flowChart]}
+              options={{
+                hAxis: {
+                  title: "Time"
+                },
+                vAxis: {
+                  title: "Money"
+                }
+              }}
+            />
+          </SummaryBox>
         </Grid>
       </Grid>
       <Modal
--- src/components/templates/admin-template/index.tsx
+++ src/components/templates/admin-template/index.tsx
@@ -5,15 +5,24 @@ import useStyles from "./styles";
 const AdminTemplate = ({ header, sidebar, content }: any) => {
   const classes = useStyles();
   return (
-    <Grid container direction="row">
-      <Grid item xs={12} sm={12} md={12} lg={12}>
-        {header}
-      </Grid>
-      <Grid item xs={12} sm={12} md={2} lg={1} className={classes.sidebar}>
-        {sidebar}
-      </Grid>
-      <Grid item xs={12} sm={12} md={10} lg={11} className={classes.content}>
-        {content}
+    <Grid container direction="column" style={{ height: "100%", flexWrap: 'nowrap' }}>
+      <Grid item>{header}</Grid>
+      <Grid item style={{ flexGrow: 1 }}>
+        <Grid container direction="row" style={{ height: "100%" }}>
+          <Grid item xs={12} sm={12} md={2} lg={1} className={classes.sidebar}>
+            {sidebar}
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={10}
+            lg={11}
+            className={classes.content}
+          >
+            {content}
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
