41d6ea9b5a961bbf6e67a15cc0aefbbce65ca281
Sinan Shaban <sinan.zikri@gmail.com>
2020-02-27 14:49:38 +0200

    Dashboard last records refetch fix

--- src/components/organisms/last-transactions/index.tsx
+++ src/components/organisms/last-transactions/index.tsx
@@ -4,20 +4,27 @@ import SummaryBox from "components/molecules/summary-box";
 import TransactionSummary from "components/molecules/transaction-summary";
 import Modal from "components/molecules/modal";
 import TransactionForm from "components/organisms/transaction-form";
-import {
-  useTransactionsLazyQuery,
-  Transaction,
-  useWalletsQuery
-} from "api";
+import { useTransactionsQuery, Transaction, useWalletsQuery } from "api";
 import Loader from "components/atoms/loader";
 import { useUpdateDetectionContext } from "services/update-detection-provider";
-import useStyles from './styles';
+import useStyles from "./styles";
 
 let backupData: any[] = [];
 
-const LastTransactions = ({ wallets, onChange }: { wallets: number[], onChange: Function }) => {
+const LastTransactions = ({
+  wallets,
+  onChange
+}: {
+  wallets: number[];
+  onChange: Function;
+}) => {
   const classes = useStyles();
-  const {setTransactionUpdate} = useUpdateDetectionContext();
+  const {
+    setTransactionUpdate,
+    lastTransactionAction,
+    lastCategoryAction,
+    lastWalletAction
+  } = useUpdateDetectionContext();
 
   const [newTransactionModalIsOpen, setNewTransactionModalIsOpen] = useState(
     false
@@ -26,19 +33,20 @@ const LastTransactions = ({ wallets, onChange }: { wallets: number[], onChange: 
   const { data: walletsData } = useWalletsQuery();
   const userWallets: any = walletsData?.wallets ?? [];
 
-  const [getTransactions, { data, refetch, loading }] = useTransactionsLazyQuery({fetchPolicy: 'network-only'});
+  const { data, refetch, loading } = useTransactionsQuery({
+    variables: {
+      date: null,
+      walletIds: wallets,
+      page: 1,
+      limit: 5,
+      unlimited: false
+    },
+    fetchPolicy: "cache-and-network"
+  });
 
   useEffect(() => {
-    getTransactions({
-      variables: {
-        date: null,
-        walletIds: wallets,
-        page: 1,
-        limit: 5,
-        unlimited: false
-      }
-    });
-  }, [getTransactions, wallets]);
+    refetch();
+  }, [wallets, lastTransactionAction, lastCategoryAction, lastWalletAction]);
 
   const transactionsData: any = data?.transactions?.data ?? null;
   if (transactionsData) {
@@ -52,10 +60,10 @@ const LastTransactions = ({ wallets, onChange }: { wallets: number[], onChange: 
       header={"Last 5 records"}
       onClick={() => setNewTransactionModalIsOpen(true)}
     >
-      <Loader loading={loading}/>
-      {
-        !transactions.length && !loading && <div className={classes.noData}>No data</div>
-      }
+      <Loader loading={loading && !transactions.length} />
+      {!transactions.length && !loading && (
+        <div className={classes.noData}>No data</div>
+      )}
       {transactions.map((transaction: Transaction, index: number) => (
         <TransactionSummary key={index} transaction={transaction} />
       ))}
@@ -70,13 +78,7 @@ const LastTransactions = ({ wallets, onChange }: { wallets: number[], onChange: 
           wallets={userWallets}
           onComplete={() => {
             setTransactionUpdate();
-            refetch({
-              date: null,
-              walletIds: wallets,
-              page: 1,
-              limit: 5,
-              unlimited: false
-            });
+            refetch();
             onChange();
             setNewTransactionModalIsOpen(false);
           }}
@@ -88,8 +90,22 @@ const LastTransactions = ({ wallets, onChange }: { wallets: number[], onChange: 
 };
 
 LastTransactions.fragment = gql`
-  query Transactions($walletIds: [Int], $page: Int, $limit: Int, $unlimited: Boolean, $date: String) {
-    transactions(input: { walletIds: $walletIds, page: $page, limit: $limit, unlimited: $unlimited, date: $date}) {
+  query Transactions(
+    $walletIds: [Int]
+    $page: Int
+    $limit: Int
+    $unlimited: Boolean
+    $date: String
+  ) {
+    transactions(
+      input: {
+        walletIds: $walletIds
+        page: $page
+        limit: $limit
+        unlimited: $unlimited
+        date: $date
+      }
+    ) {
       data {
         id
         description
